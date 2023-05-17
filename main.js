360e423038ac42fc9f60915e122b340f23f1c147
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-05-17 22:18:03 +0300

    done

--- main.js
+++ main.js
@@ -16,22 +16,30 @@ function convertion(val){
     return (val - 273).toFixed(2)
 }
 //fetch
-    btn.addEventListener('click', function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
-        .then(res => res.json())
-         //.then(data => console.log(data))
-        .then(data => {
-            var nameval = data['name']
-            var descrip = data['weather']['0']['description']
-            var tempature = data['main']['temp']
-            var wndspd = data['wind']['speed']
+   var humidity = document.querySelector("#humidity");
 
-            city.innerHTML=`City: ${nameval}`
-            temp.innerHTML = `Temperature: ${ convertion(tempature)} C`
-            description.innerHTML = `Conditions: ${descrip}`
-            wind.innerHTML = `Wind Speed: ${wndspd} km/h`
+   btn.addEventListener("click", function () {
+     fetch(
+       "https://api.openweathermap.org/data/2.5/weather?q=" +
+         inputval.value +
+         "&appid=" +
+         apik
+     )
+       .then((res) => res.json())
+       .then((data) => {
+         var nameval = data["name"];
+         var descrip = data["weather"][0]["description"];
+         var tempature = data["main"]["temp"];
+         var wndspd = data["wind"]["speed"];
+         var hum = data["main"]["humidity"];
 
-        })
-        .catch(err => alert('You entered Wrong city name'))
-    })
+         city.innerHTML = `City: ${nameval}`;
+         temp.innerHTML = `Temperature: ${convertion(tempature)} C`;
+         description.innerHTML = `Conditions: ${descrip}`;
+         wind.innerHTML = `Wind Speed: ${wndspd} km/h`;
+         humidity.innerHTML = `Humidity: ${hum}%`;
+       })
+       .catch((err) => alert("You entered the wrong city name"));
+   });
+
 
